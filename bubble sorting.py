abfeecfb896f110adc8d479f5321c1b7b66e6427
SV <VPRS6900@MAIL.RU>
2022-09-28 12:57:52 +0300

    бинарный поиск added

--- bubble sorting.py
+++ bubble sorting.py
@@ -29,5 +29,29 @@ def mas(mas):
             mas[i]=mas[index]
             mas[index]=x
     return mas
-massiv=input("Введи числа в строку через запятую: ").split(",")
-print(*mas(massiv), end=" ")
+'''massiv=input("Введи числа в строку через запятую: ").split(",")
+print(*mas(massiv), end=" ")'''
+
+#бинарный поиск
+massiv=[2,3,4,1,4,5,6,7,56,7,4,3,5,7]
+massiv=mas(massiv)
+
+n=int(input()) #искомое число
+l=0
+r=len(massiv)
+while l<r:
+    midle=(l+r) // 2
+    if massiv[midle]<n:
+        l=midle+1
+    elif massiv[midle]>n:
+        r=midle-1
+    else:
+        l=midle
+        print(l)
+        break
+
+
+
+
+
+
